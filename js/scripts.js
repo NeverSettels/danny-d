c55b44cee59b0248472ca0fb295f93ebd994cf9b
Eric Settels <settels.eric@gmail.com>
2020-03-26 11:43:29 -0700

    set up basic html and add some branching logic

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+  $('form#Danny').submit(function(event) {
+    event.preventDefault()
+    var age = parseInt($('input#age').val())
+    var penguin = $('select#penguin').val()
+    var frank = JSON.parse($('select#frank').val())
+    var twin = $('select#twin').val()
+    // frank ? console.log('yes he is') : console.log('no he isnt');
+    if (penguin === 'penguin') {
+      $('#wolv-container').removeClass('no-show')
+    } else if (penguin === 'goat') {
+      $('#mnm-container').removeClass('no-show')
+    } else if (penguin === 'neither') {
+      if (frank === true) {
+        $('#wig-container').removeClass('no-show')
+      } else {
+        $('#cartoon-container').removeClass('no-show')
+      }
+    }
+  })
+})
